abed15374811e03c38843ebeb5528dc65f0f7500
max.ross <max.ross@c65a9048-a152-11de-a69d-91aafc7ba57a>
2009-09-15 18:42:44 +0000

    BaseTestListener plus a DatastoreDelegate that tracks the kinds that have been put.

--- src/org/sortedunderbelly/appengineunit/spi/NamespacePolicy.java
+++
@@ -1,9 +0,0 @@
-package org.sortedunderbelly.appengineunit.spi;
-
-/**
- * Created by IntelliJ IDEA. User: maxr Date: Aug 12, 2009 Time: 2:19:29 PM To change this template
- * use File | Settings | File Templates.
- */
-public interface NamespacePolicy {
-  
-}
