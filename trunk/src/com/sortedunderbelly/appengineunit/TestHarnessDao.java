61e05191bfa42b6ded9aa668548827462cc6db5c
max.ross <max.ross@c65a9048-a152-11de-a69d-91aafc7ba57a>
2009-09-22 18:26:48 +0000

    Repackage into com.sortedunderbelly. Added a build script. Added one unit test.

---
+++ trunk/src/com/sortedunderbelly/appengineunit/TestHarnessDao.java
@@ -0,0 +1,34 @@
+package com.sortedunderbelly.appengineunit;
+
+import com.sortedunderbelly.appengineunit.model.Failure;
+import com.sortedunderbelly.appengineunit.model.Run;
+import com.sortedunderbelly.appengineunit.model.Test;
+
+/**
+ * @author Max Ross <maxr@google.com>
+ */
+public interface TestHarnessDao {
+  Run newRun();
+
+  void updateRun(Run run);
+
+  Run findRunById(long runId);
+
+  int getNumTestsStartedForRun(long runId);
+
+  int getNumTestsInProgressForRun(long runId);
+
+  int getNumTestsFailedForRun(long runId);
+
+  Test newTest(Run run, String testId);
+
+  void updateTest(Test test);
+
+  Iterable<Failure> getFailuresForRun(long runId);
+
+  /**
+   * @return {@code true} if the completion record was created, {@code false}
+   * otherwise.
+   */
+  boolean createCompletionRecordIfNotAlreadyPresent(long runId);
+}
